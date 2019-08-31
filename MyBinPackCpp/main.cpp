6df3df0a03a3f9bf6af85548e83d1b2a1b7fc5ff
15821912398 <bennett_tri@sjtu.edu.cn>
2019-08-31 14:23:40 +0800

    initialize

---
+++ MyBinPackCpp/main.cpp
@@ -0,0 +1,55 @@
+#include "Vehicle.h"
+#include "Bin.h"
+#include "Station.h"
+#include <iostream>
+#include <map>
+#include "util.h" //工具函数
+#include "rapidjson\document.h"
+#include "rapidjson\writer.h"
+#include "rapidjson\stringbuffer.h"
+using namespace rapidjson;
+
+
+
+int main() {
+	cout << my_util::id_to_num("S005") << endl;
+	vector<int> a = { 3,3,4,5 };
+	vector<int> b = a;
+	b.at(0) = 7;
+	cout << "变了吗？" << a.at(0) << endl;
+	cout << "变了吗？" << b.at(0) << endl;
+
+	// 导入数据
+	//vector<Bin> bins = my_util::get_bins_data();  //bins[i] 即为 "B(i+1)"
+	//vector<Vehicle> vehicles = my_util::get_vehicles_data();
+	//vector<Station> stations = my_util::get_stations_data();
+	//double distance_matrix[num_stations][num_stations];  //"s005到s004的距离为：distance_matrix[4-1][5-1] 顺序是反的" 
+	//double load_time_matrix[num_stations][num_stations]; //"s005到s004的运输时间为：load_time_matrix[4-1][5-1]" 
+	//my_util::get_distance_matrix(distance_matrix);
+	//my_util::get_load_time_matrix(load_time_matrix);
+
+	////cout << load_time_matrix[4][5] << endl;
+	////cout << stations.at(4).get_load_time() << endl;
+	//vector<Vehicle> used_vehicles;
+	//vector<Vehicle> unused_vehicles = vehicles;
+
+	//
+	//vector<Station>::iterator sit = stations.begin();
+	//while (sit != stations.end()) {
+	//	vector<Bin>::iterator bit = bins.begin();
+	//	while (bit != bins.end()) {
+	//		if ((*sit).get_id() == (*bit).get_station()) {
+	//			(*sit).own_bins.push_back((*bit));
+	//		}
+	//		bit++;
+	//	}
+	//	sit++;
+	//}
+
+	//cout << stations.at(5).own_bins[2] << endl;
+
+
+
+	getchar();
+	return 0;
+}
---
+++ MyBinPackCpp/util.h
@@ -0,0 +1,165 @@
+#pragma once
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <string>
+#include "rapidjson\document.h"
+#include "rapidjson\writer.h"
+#include "rapidjson\stringbuffer.h"
+using namespace rapidjson;
+using namespace std;
+const int num_stations = 215;  // 站点数目
+
+
+namespace my_util{
+
+	
+	//从文件读入到string里
+	char* readFileIntoString(char * filename)
+	{
+		ifstream f(filename); //taking file as inputstream
+		string str;
+		if (f) {
+			ostringstream ss;
+			ss << f.rdbuf(); // reading data
+			str = ss.str();
+		}
+		return _strdup(str.c_str());
+	}
+
+	vector<Bin> get_bins_data(){
+		char* bin_json = my_util::readFileIntoString("month3\\bin.json");
+		Document d;
+		d.Parse(bin_json);
+		const Value& a = d["Bin"];
+		vector<Bin> bins;
+		for (SizeType i = 0; i < a.Size(); i++) {// 使用 SizeType 而不是 size_t
+			Bin *temp = new Bin(a[i]["bin_id"].GetString(),
+				a[i]["bin_width"].GetDouble(),
+				a[i]["bin_length"].GetDouble(),
+				a[i]["bin_weight"].GetDouble(),
+				a[i]["station"].GetString());
+			bins.push_back(*temp);
+		}
+
+		return bins;
+	}
+
+	vector<Vehicle> get_vehicles_data() {
+		char* vehicle_json = my_util::readFileIntoString("month3\\vehicle.json");
+		Document d;
+		d.Parse(vehicle_json);
+		const Value& a = d["Vehicle"];
+		vector<Vehicle> vehicles;
+		for (SizeType i = 0; i < a.Size(); i++) {// 使用 SizeType 而不是 size_t
+			Vehicle *temp = new Vehicle(a[i]["vehicle_id"].GetString(),
+				a[i]["vehicle_width"].GetDouble(),
+				a[i]["vehicle_length"].GetDouble(),
+				a[i]["vehicle_weight"].GetDouble(),
+				a[i]["flag_down_fare"].GetDouble(),
+				a[i]["distance_fare"].GetDouble());
+			vehicles.push_back(*temp);
+		}
+
+		return vehicles;
+	}
+
+	vector<Station> get_stations_data() {
+		char* station_json = my_util::readFileIntoString("month3\\station.json");
+		Document d;
+		d.Parse(station_json);
+		const Value& a = d["Station"];
+		vector<Station> stations;
+		for (SizeType i = 0; i < a.Size(); i++) {// 使用 SizeType 而不是 size_t
+			//Station(string id, double limit, double load_time);
+			Station *temp = new Station(a[i]["station_id"].GetString(),
+				a[i]["vehicle_limit"].GetDouble(),
+				a[i]["loading_time"].GetDouble());
+			stations.push_back(*temp);
+		}
+
+		return stations;
+	}
+
+	void get_distance_matrix(double distance_matrix[num_stations][num_stations]) {
+		char* matrix_json = my_util::readFileIntoString("month3\\matrix.json");
+		Document d;
+		d.Parse(matrix_json);
+		const Value& a = d["Matrix"];
+		for (SizeType i = 0; i < a.Size(); i++) {// 使用 SizeType 而不是 size_t
+			distance_matrix[i / num_stations][i % num_stations] = a[i]["distance"].GetDouble();
+		}
+		for (size_t i = 0; i < num_stations; i++){
+			distance_matrix[i][i] = 0.0;
+		}
+	}
+
+	void get_load_time_matrix(double load_time_matrix[num_stations][num_stations]) {
+		char* matrix_json = my_util::readFileIntoString("month3\\matrix.json");
+		Document d;
+		d.Parse(matrix_json);
+		const Value& a = d["Matrix"];
+		for (SizeType i = 0; i < a.Size(); i++) {// 使用 SizeType 而不是 size_t
+			load_time_matrix[i / num_stations][i % num_stations] = a[i]["time"].GetDouble();
+		}
+		for (size_t i = 0; i < num_stations; i++) {
+			load_time_matrix[i][i] = 0.0;
+		}
+	}
+
+	//获取数组长度
+	template <class T>
+	int getArrayLen(T& array)
+	{
+		return (sizeof(array) / sizeof(array[0]));
+	}
+
+	//初始化时选车
+	Vehicle& pick_vehicle(vector<Vehicle>& unused_vehicles, Station& station) {
+		vector<Vehicle>::iterator vit = unused_vehicles.begin();
+		while (vit != unused_vehicles.end()) {
+			if (vit->get_length() <= station.get_limit())
+				return *(unused_vehicles.erase(vit));
+			vit++;
+		}
+	}
+
+	// 将站点ID(或车辆ID)转化为序号，如"S007" -> 6
+	int id_to_num(string id) {
+		return stoi(id.substr(1)) - 1;
+	}
+
+	//计算路程
+	double route_distance(vector<Station>& route, double *distance_matrix[num_stations]) {
+		double d = 0;
+		for(size_t i = 0; i <route.size() - 1; i++)
+		{
+			d += distance_matrix[id_to_num(route.at(i + 1).get_id())][id_to_num(route.at(i).get_id())];
+		}
+		return d;
+	}
+
+	//计算总用时
+	double compute_total_time(vector<Station>& route, double *load_time_matrix[num_stations]) {
+		double total_time = 0;
+
+		for each(Station s in route) {
+			total_time += s.get_load_time();
+		}
+		for (size_t i = 0; i <route.size() - 1; i++)
+		{
+			total_time += load_time_matrix[id_to_num(route.at(i + 1).get_id())][id_to_num(route.at(i).get_id())];
+		}
+		return total_time;
+	}
+
+	//计算把j 并入i之后的最佳路线及成本的节省
+	void compute_route(Vehicle i, Vehicle j, double *distance_matrix[num_stations]) {
+		double cost_ori = j.get_flagdown_fare() + j.get_distance_fare() * route_distance(j.visit_order, distance_matrix) + \
+			i.get_distance_fare() * route_distance(i.visit_order, distance_matrix);
+		vector<Station> temp_vo = i.visit_order;
+		temp_vo.insert(temp_vo.end(), j.visit_order.begin(), j.visit_order.end());
+		unique(temp_vo.begin(), temp_vo.end());
+	}
+
+}
